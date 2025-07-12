b6b6b3953eaa8a63ff97e0ca91a8fd0dbaac6448
Vangaren1 <markthompson6807@gmail.com>
2025-07-11 23:36:47 -0400

    got a 100% pass rate

--- Deque.java
+++ Deque.java
@@ -7,15 +7,120 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-
 public class Deque<Item> implements Iterable<Item> {
+
+    private Node first, last;
+    private int size;
+
+    // construct an empty deque
+    public Deque() {
+        size = 0;
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return size;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+
+        Node tmp = new Node();
+        tmp.val = item;
+        if (size == 0) {
+            first = tmp;
+            last = tmp;
+        } else {
+            tmp.next = first;
+            first.prev = tmp;
+            first = tmp;
+        }
+        size++;
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+        if (item == null) throw new IllegalArgumentException();
+
+        Node tmp = new Node();
+        tmp.val = item;
+        if (size == 0) {
+            first = tmp;
+            last = tmp;
+        } else {
+            tmp.prev = last;
+            last.next = tmp;
+            last = tmp;
+        }
+        size++;
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        if (size == 0) throw new NoSuchElementException();
+
+        Node tmp = first;
+        first = first.next;
+        if (first != null) first.prev = null;
+        size--;
+
+        if (size == 0) {
+            first = null;
+            last = null;
+        }
+
+        tmp.next = null;
+        tmp.prev = null;
+
+        return tmp.val;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        if (size == 0) throw new NoSuchElementException();
+
+        Node tmp = last;
+        last = last.prev;
+        if (last != null) last.next = null;
+        size--;
+
+        if (size == 0) {
+            first = null;
+            last = null;
+        }
+
+        tmp.prev = null;
+        tmp.next = null;
+        return tmp.val;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    private class Node {
+        private Item val;
+        private Node next;
+        private Node prev;
+    }
 
     private class DequeIterator implements Iterator<Item> {
 
-        int pos = first;
-        int count = 0;
+        Node curr;
+        public DequeIterator() {
+            curr = first;
+        }
+
         public boolean hasNext() {
-            return count < queueSize;
+            if (last == null) return false;
+            return curr != last.next;
         }
 
         public void remove() {
@@ -25,144 +130,26 @@ public class Deque<Item> implements Iterable<Item> {
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
 
-            Item tmp = itemArray[pos];
-            pos = (pos + capacity + 1) % capacity;
-            count++;
+            Item tmp = curr.val;
+            curr = curr.next;
 
             return tmp;
         }
+
+
     }
 
-    private int queueSize;
-    private int capacity;
-    private int first, last;
-    private Item[] itemArray;
-
-    // construct an empty deque
-    public Deque() {
-        capacity = 1;
-        itemArray = (Item[]) new Object[capacity];
-        queueSize = 0;
-        this.first = 0;
-        this.last = 0;
-    }
-
-    private void resize(int cap) {
-        if (cap < queueSize) throw new IndexOutOfBoundsException();
-
-        Item[] newArray = (Item[]) new Object[cap];
-        for (int index = 0; index < queueSize; index++) {
-            newArray[index] = itemArray[(first + capacity + index) % capacity];
-        }
-        itemArray = newArray;
-        this.capacity = cap;
-        first = 0;
-        last = queueSize-1;
-    }
-
-    private void reset() {
-        int cap = 1;
-        itemArray = (Item[]) new Object[cap];
-        first = 0;
-        last = 0;
-        this.capacity = cap;
-    }
-
-    // is the deque empty?
-    public boolean isEmpty() {
-        return queueSize == 0;
-    }
-
-    // return the number of items on the deque
-    public int size() {
-        return queueSize;
-    }
-
-    // add the item to the front
-    public void addFirst(Item item) {
-
-        if (item == null) throw new IllegalArgumentException();
-
-        if (queueSize == capacity) resize(capacity * 2);
-
-        if (queueSize == 0) {
-            itemArray[0] = item;
-        } else {
-            first = (first + capacity - 1) % capacity;
-            itemArray[first] = item;
-        }
-        queueSize++;
-    }
-
-
-    // add the item to the back
-    public void addLast(Item item) {
-
-        if (item == null) throw new IllegalArgumentException();
-
-        if (queueSize == capacity) resize(capacity * 2);
-
-        if (queueSize == 0) {
-            itemArray[0] = item;
-        } else {
-            last = (last + capacity + 1) % capacity;
-            itemArray[last] = item;
-        }
-        queueSize++;
-    }
-
-    // remove and return the item from the front
-    public Item removeFirst() {
-        if (queueSize == 0) throw new NoSuchElementException();
-
-        Item tmp = itemArray[first];
-        itemArray[first] = null;
-        first = (first + capacity + 1) % capacity;
-        queueSize--;
-
-        if (queueSize == 0) {
-            reset();
-        } else {
-            if (queueSize <= (capacity / 4)) resize(capacity/2);
-        }
-
-        return tmp;
-    }
-
-    // remove and return the item from the back
-    public Item removeLast() {
-        if (queueSize == 0) throw new NoSuchElementException();
-
-        Item tmp = itemArray[last];
-        itemArray[last] = null;
-        last = (last + capacity - 1) % capacity;
-        queueSize--;
-
-        if (queueSize == 0) {
-            reset();
-        } else {
-            if (queueSize <= (capacity / 4)) resize(capacity/2);
-        }
-
-        return tmp;
-    }
-
-    // return an iterator over items in order from front to back
-    public Iterator<Item> iterator() {
-        return new DequeIterator();
-    }
-
+    // unit testing (required)
     public static void main(String[] args) {
-        Deque<Integer> deque = new Deque<>();
-        System.out.println(deque.isEmpty());
-        deque.addFirst(2);
-        deque.addFirst(3);
-        System.out.println(deque.removeFirst());
-        System.out.println(deque.removeFirst());
-        System.out.println(deque.isEmpty());
-        System.out.println(deque.isEmpty());
-        System.out.println(deque.isEmpty());
-        deque.addFirst(9);
-        System.out.println(deque.removeFirst());
+        Deque<String> deque = new Deque<>();
+        deque.addFirst("IBATXMGMSB");
+        deque.addLast("JIXEHETMDN");
+        deque.addFirst("EPVLOOBTGN");
+        deque.addLast("XGEEXRYBOL");
+        deque.removeLast();
+        deque.removeLast();
+        deque.removeFirst();
+        deque.removeLast();
     }
+
 }
